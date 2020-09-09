7162309ad5f7bd717be71c7ec8126c32ca5b8bce
buitim <buitim@oregonstate.edu>
2020-09-08 20:10:57 -0700

    Added discord widget

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Typography, Divider } from 'antd';
+import { Typography } from 'antd';
 import { withRouter } from 'react-router-dom';
-const { Title, Paragraph, Text } = Typography;
+const { Title } = Typography;
 
 export class HomeView extends React.Component{
     constructor(props) {
@@ -18,11 +18,12 @@ export class HomeView extends React.Component{
     
     render() {
         return(
-            <Typography>
-                <Title>Welcome To TrashHub</Title>
-                <Paragraph>This is a paragraph. <Text code>This is some code</Text></Paragraph>
-                <Divider/>
-            </Typography>
+            <>
+                <Typography>
+                    <Title>Welcome To TrashHub</Title>
+                </Typography>
+                <iframe title='Discord Widget' src="https://discordapp.com/widget?id=507367480268947456&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            </>
         );
     }
 }
